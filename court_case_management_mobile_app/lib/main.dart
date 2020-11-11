5cbc3be8754b17bedba45d4a09a3362b01a12b8b
Poujhit <poujhit112313@gmail.com>
2020-11-11 21:02:15 +0530

    initial commit for mobile app

---
+++ court_case_management_mobile_app/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'COURT CASE MANAGEMENT',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('COURT CASE MANAGEMENT'),
+      ),
+      body: Center(child: Text('hello world')),
+    );
+  }
+}
