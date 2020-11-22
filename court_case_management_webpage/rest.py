dcda402812c163462cc8dda60b1e3711bfbd06a3
NaveenSriram-dev <62332925+NaveenSriram-dev@users.noreply.github.com>
2020-11-22 12:45:23 +0530

    sql integrated

--- court_case_management_webpage/rest.py
+++ court_case_management_webpage/rest.py
@@ -1,5 +1,6 @@
-from flask_restful import Resource, Api
+from flask_restful import Resource
 import mysql.connector
+from json import dumps , loads
 #################################
 ###### Database Connection ######
 #################################
@@ -12,7 +13,7 @@ config = {
 }
 
 db = mysql.connector.connect(**config)
-mycursor = db.cursor(buffered=True)
+cursor = db.cursor(buffered=True , dictionary = True)
 
 #################################
 ########  API classes  ##########
@@ -21,25 +22,46 @@ mycursor = db.cursor(buffered=True)
 class  Caselist(Resource):
 
     def get(self,uid):   # Returns all the cases of that User id
-        pass
+        sql = "SELECT id, fir_no FROM cases WHERE u_id ="+uid+";"
+        cursor.execute(sql)
+        cases = cursor.fetchall()
+        cases = dumps(cases)
+        return cases
 
     def post(self,uid):   # Used to add another case with User id
         pass
 
 class Hearings(Resource):
     def get(self,cid):    # Returns all hearings of that Case id
-        pass
+        sql = "SELECT * FROM hearings WHERE cid ="+cid+";"
+        cursor.execute(sql)
+        hearings= cursor.fetchall()
+        hearings = dumps(hearings)
+        return hearings
 
     def post(self,cid):   # Used to add a hearing to a case
+        sql = "INSERT INTO hearings (date , judge, start ,end ,location, next, c_id ) VALUES ( ) ;"
+        cursor.execute(sql)
+        db.commit()
         pass
 
 class Case(Resource):
     def get(self,cid):    # Returns the case details using Case id
-        pass
+        sql = "SELECT * FROM cases WHERE id ="+cid+";"
+        cursor.execute(sql)
+        case = cursor.fetchall()
+        case = dumps(case)
+        return case
 
-    def patch(self,cid,verdict): # Used to update the verdict of a case
-        pass
+    def patch(self,cid): # Used to update the verdict of a case
+        sql = "UPDATE cases SET status = '"++"' verdict = '"++"' WHERE c_id = "++";"
+        cursor.execute(sql)
+        db.commit()
 
 class Lawyers(Resource):
     def get(self):        # Returns all Lawyers
-        pass
+        sql = "SELECT id , name FROM lawyers ;"
+        cursor.execute(sql)
+        lawyers = cursor.fetchall()
+        lawyers= json.dumps(lawyers)
+        return lawyers
