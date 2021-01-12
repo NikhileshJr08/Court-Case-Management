40800bc8559674fe6178ed0ae1aaaf03df491e52
Poujhit <poujhit.dev@gmail.com>
2021-01-12 15:38:42 +0530

    Feat: added modal for adding new cases

--- court_case_management_mobile_app/lib/screens/profile_screen.dart
+++ court_case_management_mobile_app/lib/screens/profile_screen.dart
@@ -1,3 +1,4 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gradients/flutter_gradients.dart';
@@ -5,8 +6,14 @@ import 'package:flutter_gradients/flutter_gradients.dart';
 import '../widgets/my_cases_widget.dart';
 import '../widgets/bottom_navbar.dart';
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
   static const routeName = '/ProfileScreen';
+
+  @override
+  _ProfileScreenState createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -98,7 +105,145 @@ class ProfileScreen extends StatelessWidget {
           ],
         ),
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showGeneralDialog(
+              barrierDismissible: true,
+              barrierLabel: 'hello',
+              context: context,
+              pageBuilder: (ctx, _, __) {
+                return Dialog(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ), //this right here
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(top: 8),
+                        alignment: Alignment.center,
+                        child: AutoSizeText(
+                          'Add A New Case',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          textAlign: TextAlign.center,
+                          maxLines: 2,
+                        ),
+                      ),
+                      addFieldsForAddingCase(
+                        leadingTitle: 'Case Id:',
+                      ),
+                      addFieldsForAddingCase(
+                        leadingTitle: 'Case Name:',
+                      ),
+                      addFieldsForAddingCase(
+                        leadingTitle: 'Party Name:',
+                      ),
+                      addFieldsForAddingCase(
+                        leadingTitle: 'FIR No. : ',
+                      ),
+                      addFieldsForAddingCase(
+                        leadingTitle: 'Prosecutor:',
+                      ),
+                      addFieldsForAddingCase(
+                        leadingTitle: 'Defendant:',
+                      ),
+                      addFieldsForAddingCase(
+                        leadingTitle: 'Defense Lawyer:',
+                      ),
+                      addFieldsForAddingCase(
+                        leadingTitle: 'Status:',
+                      ),
+                      addFieldsForAddingCase(
+                        leadingTitle: 'Case Type:',
+                      ),
+                      addFieldsForAddingCase(
+                        leadingTitle: 'Verdict:',
+                      ),
+                      Divider(thickness: 1),
+                      GestureDetector(
+                        onTap: () => Navigator.of(ctx).pop(),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Expanded(
+                              child: Container(
+                                margin: EdgeInsets.only(
+                                  bottom: 5,
+                                ),
+                                child: FlatButton(
+                                  onPressed: () => Navigator.of(ctx).pop(),
+                                  child: Text(
+                                    'OK',
+                                    style: TextStyle(
+                                      fontFamily: 'Poppins',
+                                      color: Colors.blue,
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              }).then((value) {
+            setState(() {});
+          });
+        },
+        tooltip: 'Add a New Case',
+        child: Icon(
+          Icons.add,
+        ),
+      ),
       bottomNavigationBar: BottomBar(),
+    );
+  }
+
+  Row addFieldsForAddingCase({
+    String leadingTitle,
+    TextEditingController controller,
+  }) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Expanded(
+          flex: 2,
+          child: Container(
+            margin: const EdgeInsets.only(left: 5, right: 3),
+            alignment: Alignment.center,
+            child: AutoSizeText(
+              leadingTitle,
+              style: TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+              maxLines: 2,
+            ),
+          ),
+        ),
+        Expanded(
+          flex: 4,
+          child: Container(
+            margin: const EdgeInsets.only(
+              right: 20,
+            ),
+            child: TextField(
+              controller: controller,
+            ),
+          ),
+        )
+      ],
     );
   }
 
